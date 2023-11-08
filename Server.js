cc9e13191d02f7eef7a1e4cd70b1911393746739
Salmanul-faris1696 <109976353+Salmanul-faris1696@users.noreply.github.com>
2023-11-08 10:07:03 +0530

    Add files via upload
    backend

---
+++ Server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const routes = require('./routes/ToDoRoutes');
+
+require('dotenv').config();
+
+const app =express()
+const PORT = process.env.PORT || 5000
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+//connect mongoose
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then( () => {
+        console.log ('connected into mongodb successfully !!!')
+    
+    } )
+    .catch( (error) => console.log(error));
+
+app.use(routes)
+
+app.listen( PORT, ()=> console.log(`listening on :${PORT}`))
+
+
+
+
+
+
+
+
+
